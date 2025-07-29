460a220d65d532735fd77bd2345e5312ff65a33e
Efanier <programacion@inteligenciadedatos.com>
2025-07-29 18:28:57 -0500

    Modifico el mensaje de presentacion

--- presentacion.py
+++ presentacion.py
@@ -1,4 +1,4 @@
 nombre = "Luis EFanier Sánchez Echavarria"
 edad = 31
 lenguaje = "Python"
-print(f"Hola, mi nombre es {nombre}, tengo {edad} años y estoy aprendiendo {lenguaje}")
+print(f"Hola, mi nombre es {nombre}, tengo {edad} años y estoy aprendiendo {lenguaje}. Hola estoy modificando")
